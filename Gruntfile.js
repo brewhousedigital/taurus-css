a2d99b05453f603ab931abe1282750f396e726ea
Brewhouse Digital <zach@brewhouse.digital>
2020-10-01 20:14:26 -0500

    removed rogue LG column classes

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,6 +21,9 @@ module.exports = function(grunt) {
         },
 
         cssmin: {
+            options: {
+
+            },
             css: {
                 src: 'styles.css',
                 dest: 'styles.min.css'
